d740079cb9e86bfba91cc61773bfb856e6b353fe
Mark Miranda <notmarkmiranda@gmail.com>
2016-11-25 18:30:52 -0700

    cant figure out sideNav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
-import { MaterializeDirective } from 'angular2-materialize'
+import { MaterializeDirective } from 'angular2-materialize';
 
 import "materialize-css";
 import "angular2-materialize";
