4513036a8c0795909e4d1b86b471f3ec193f2f87
Mark Miranda <notmarkmiranda@gmail.com>
2016-12-13 20:58:17 -0700

    players index page

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from './player.service'
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
-  styleUrls: ['./players.component.scss']
+  providers: [PlayerService]
 })
 export class PlayersComponent implements OnInit {
+  private players;
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.getPlayers();
   }
 
+  getPlayers() {
+    return this.playerService.get().then(players => {
+      this.players = players;
+    });
+  }
 }
