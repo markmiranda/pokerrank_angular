74e7576c1c511d4726b515d434ddd14e43d61b65
Mark Miranda <notmarkmiranda@gmail.com>
2017-01-09 17:35:16 -0700

    update

--- src/app/players/player-detail.service.ts
+++ src/app/players/player-detail.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Resposne } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { environment } from '../../environments/environment';
 
 @Injectable()
